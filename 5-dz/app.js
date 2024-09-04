1cd13f6bdab5045efb70e8457f771dc45a9837c7
DinaKobrik <dopkobrik@mail.ru>
2024-09-04 10:56:45 +0300

    5-language

---
+++ 5-dz/app.js
@@ -0,0 +1,15 @@
+const language = "fi";
+
+switch (language) {
+  case "ru":
+    console.log("Здравствуй!");
+    break;
+  case "de":
+    console.log("Gutten tag!");
+    break;
+  case "fi":
+    console.log("Hei!");
+    break;
+  default:
+    console.log("Hello!");
+}
