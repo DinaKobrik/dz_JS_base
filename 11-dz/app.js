6f3cbd4145dbad8cd36ef1e001664347ece24a21
DinaKobrik <dopkobrik@mail.ru>
2024-09-12 19:19:19 +0300

    correct

--- 11-dz/app.js
+++ 11-dz/app.js
@@ -1,17 +1,50 @@
-const arr = ["10-02-2022", "тест", "11/12/2023", "00/13/2022", "41/12/2023"];
+const arr = [
+  "10-02-2022",
+  "29-02-2022",
+  "29-02-2024",
+  "30-04-2022",
+  "18.04.2022",
+  "тест",
+  "11/12/2023",
+  "00/13/2022",
+  "41/12/2023",
+];
 
 function check(array) {
   const arrNew = [];
+
   for (let i = 0; i < array.length; i++) {
-    let dateParts = array[i].includes("-")
-      ? array[i].split("-")
-      : array[i].split("/");
-    const [day, month, year] = dateParts.map(Number);
-    if (year > 0 && month > 0 && month <= 12 && day > 0 && day <= 31) {
-      arrNew.push(`${day}-${month}-${year}`);
+    let dateParts = [];
+    let format = "";
+    if (array[i].includes("-") || array[i].includes(".")) {
+      dateParts = array[i].includes("-")
+        ? array[i].split("-")
+        : array[i].split(".");
+      format = "dd-mm-yyyy";
+    } else if (array[i].includes("/")) {
+      dateParts = array[i].split("/");
+      format = "mm-dd-yyyy";
+    }
+    let [day, month, year] = dateParts.map(Number);
+    if (year > 0 && month > 0 && month <= 12 && day > 0) {
+      const shortMonths = [4, 6, 9, 11];
+      const isLeapYear = year % 4 === 0;
+      if (
+        (month === 2 && (isLeapYear ? day <= 29 : day <= 28)) ||
+        (shortMonths.includes(month) && day <= 30) ||
+        (!shortMonths.includes(month) && month !== 2 && day <= 31)
+      ) {
+        if (format === "dd-mm-yyyy") {
+          arrNew.push(`${day}-${month}-${year}`);
+        } else if (format === "mm-dd-yyyy") {
+          arrNew.push(`${month}-${day}-${year}`);
+        }
+      }
     }
   }
+
   return arrNew;
 }
 
 console.log(check(arr));
+
--- 12-dz/app.js
+++ 12-dz/app.js
@@ -1,15 +1,25 @@
-const card = "4567-2378-4309-8740";
-
 function luna(cardNum) {
   const cardMass = cardNum.split("-").join("").split("").map(Number);
+  if (cardNum.includes(NaN)) {
+    return NaN;
+  }
   for (let i = 0; i < cardMass.length; i += 2) {
-    cardMass[i] = cardMass[i] * 2;
-    if (cardMass[i] > 9) {
-      cardMass[i] -= 9;
-    }
+    cardMass[i] = cardMass[i] > 9 ? (cardMass[i] -= 9) : cardMass[i] * 2;
   }
   const sum = cardMass.reduce((acc, num) => acc + num, 0);
   return sum % 10 === 0;
 }
 
-console.log(luna(card));
+const card = "234s834503458353";
+const card1 = "2342834503458353";
+const card2 = "4561-2612-1234-5464";
+const card3 = "4561-2612-1534-5464";
+const card4 = "4567-2378-4309-8740";
+
+function resultTemplate(cardNum) {
+  return `Карта с номером: ${cardNum} получила результат: ${luna(cardNum)}`;
+}
+console.log(resultTemplate(card));
+console.log(resultTemplate(card1));
+console.log(resultTemplate(card2));
+console.log(resultTemplate(card3));
